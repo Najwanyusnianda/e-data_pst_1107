0da00323e907eb9ab682ee0b37aaa01b034acdaa
Najwanyusnianda <najwanyusnianda@gmail.com>
2020-03-29 09:04:06 +0700

    added auth

--- app/Publikasi.php
+++ app/Publikasi.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Publikasi extends Model
 {
     //
+    protected $primaryKey = 'pub_id';
+
     protected $fillable=['pub_id','title','issn','release_date','update_date','size','cover','pdf'];
 }

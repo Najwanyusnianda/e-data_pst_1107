8402f3ef08517f60b72932b52e9f80db4e7e9a13
Najwanyusnianda <najwanyusnianda@gmail.com>
2020-03-29 09:21:07 +0700

    added auth

--- app/Publikasi.php
+++ app/Publikasi.php
@@ -8,6 +8,7 @@ class Publikasi extends Model
 {
     //
     protected $primaryKey = 'pub_id';
+    public $incrementing = false;
 
     protected $fillable=['pub_id','title','issn','release_date','update_date','size','cover','pdf'];
 }
